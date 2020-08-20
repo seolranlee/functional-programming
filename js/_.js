62b560f5ca1414c08238d91ae25de8367ad13626
seolranlee <leesugar90@gmail.com>
2020-08-20 16:19:12 +0900

    reduce

--- js/_.js
+++ js/_.js
@@ -1,3 +1,30 @@
+// 3. 커링
+// 1. _curry, _curryr(오른쪽에서부터 인자를 적용해 나가는 curryr) (함수를 값으로 들고 있다가 원하는 시점까지 미뤄뒀다가 최종적으로 평가하는 기법)
+function _curry(fn) {
+    return function (a, b) {
+        // 인자가 2개 들어오면 한번에 즉시 평가.
+        // 인자가 하나만 들어오면 안쪽에 함수를 리턴해서 한번 더 평가시점을 미루는 형식.
+        return arguments.length == 2 ? fn(a, b) : function (b) { return fn(a, b) }
+    }
+}
+
+// 인자를 맨 오른쪽에서부터 적용해 나간다.
+function _curryr(fn) {
+    return function (a, b) {
+        return arguments.length == 2 ? fn(a, b) : function (b) { return fn(b, a) } // 인자를 맨 오른쪽에서부터 적용해 나간다.
+    }
+}
+
+// 커리함수
+// 인자로 본체 함수를 받고
+// 커리 함수를 실행하면 즉시 함수를 리턴
+
+// 2. _get 만들어 좀 더 간단하게 하기
+// object에 있는 값을 안전하게 참조 
+var _get = _curryr(function (obj, key) {
+    return obj == null ? undefined : obj[key]
+})
+
 // 2. _filter, _map으로 리팩토링
 function _filter(list, predi) {
     var new_list = []
@@ -21,4 +48,22 @@ function _each(list, iter) {  // 해당 i번째 값을 순회하는 함수
         iter(list[i])   // for문을 돌면서 안에서 하는 일을 완전히 위임
     }
     return list
+}
+
+// 4. _reduce 만들기
+var slice = Array.prototype.slice
+function _rest(list, num) {
+    return slice.call(list, num || 1)
+}
+function _reduce(list, iter, memo) {    // memo: 시작 값
+    // return iter(iter(iter(0, 1), 2), 3)
+    if (arguments.length == 2) {
+        memo = list[0]
+        // slice는 Array의 "메소드"이기 때문에 _reduce함수는 Array의 의존적인 함수가 되버린다.
+        list = _rest(list)
+    }
+    _each(list, function (val) {
+        memo = iter(memo, val)
+    })
+    return memo
 }
